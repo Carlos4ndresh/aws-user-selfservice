936176c9f6abc89ade313c30c5dfed6be4468c3a
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2020-03-08 11:37:48 -0500

    tested initial group creation in terraform, testet policy attachment, pending auto-user creation from a list file (if possible)

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,5 +1,52 @@
-provider "aws" {
-    region = var.region
-    version = "~>2.0"
+resource "aws_iam_group" "interns" {
+  name = "interns"
+}
+
+resource "aws_iam_group" "mentors" {
+    name = "mentors"
+}
+
+resource "aws_iam_group" "interns-devops" {
+    name = "interns-devops"
+}
+
+resource "aws_iam_group" "developers" {
+    name = "developers"
+}
+
+resource "aws_iam_group" "non_privileged" {
+    name = "non_privileged_users"
+}
+
+# resource "aws_iam_group" "qa_engineers" {
+#     name = "qa_engineers"
+#     path = "/users/qa_engineers/"
+# }
+
+resource "aws_iam_policy" "UserSelfManagement" {
+  name        = "IAMUser_SelfManagement"
+  path        = "/"
+  description = "Password and account Policy for non-administrator users, interns and mentors"
+
+  policy = file("./iam/policies/Non_PrivilegedUsers_IAM_Selfmanagement.json")
+
+}
+
+resource "aws_iam_policy" "UserMFASelfmanagement" {
+  name        = "IAMUser_MFASelfManagement"
+  path        = "/"
+  description = "MFA Policy for non-administrator users, interns and mentors"
+
+  policy = file("./iam/policies/Non_PrivilegedUsers_MFA_Policy.json")
+}
+
+resource "aws_iam_group_policy_attachment" "NonPrivilegedAttachment" {
+    group = aws_iam_group.non_privileged.id
+    policy_arn = aws_iam_policy.UserSelfManagement.arn
+}
+
+resource "aws_iam_group_policy_attachment" "NonPrivilegedMFAAttachment" {
+    group = aws_iam_group.non_privileged.id
+    policy_arn = aws_iam_policy.UserMFASelfmanagement.arn
 }
 
