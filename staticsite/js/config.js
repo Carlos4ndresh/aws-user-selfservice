f82eb4efa8c909fddbe94127649efd029cb9a28a
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2020-03-08 09:15:59 -0500

    added terraform starter files for group and policies creation

--- staticsite/js/config.js
+++
@@ -1,5 +0,0 @@
-var userPoolId =  "us-east-1_94X9jU6uQ", // e.g. us-east-2_uXboG5pAb
-var userPoolClientId = "49p6h2olqm6etefk53iih0m7jh", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-var region = "us-east-1" // e.g. us-east-2
-
- 
