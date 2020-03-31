4fe32a33435c6d4e7a266162e711f6d4e206f2ba
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2020-03-31 12:46:51 -0500

    added vpc,ec2 modules to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,21 @@ module "lambda" {
     user_creation_lambda_role = module.iam.user_creation_lambda_role
     provisioner = var.provisioner
 }
+
+module "vpc" {
+    source = "./vpc"
+    project = var.project
+    provisioner = var.provisioner
+    owner = var.owner
+    env = var.env
+}
+
+module "ec2" {
+    source = "./ec2bastion"
+    project = var.project
+    provisioner = var.provisioner
+    owner = var.owner
+    env = var.env
+    key_pair = var.key_pair
+    security_group_id = var.security_group_id
+}
