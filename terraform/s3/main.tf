d5061cd4d330c22e4a4677b5870aed5f6d5cc71a
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2020-04-01 16:45:51 -0500

    added deafault SSE to s3 delivery bucket

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,7 +1,21 @@
+resource "aws_kms_key" "s3_encryption_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "delivery_bucket" {
   bucket = "endava-med-iam-file-delivery-bucket"
   acl    = "private"
   region = var.region
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.s3_encryption_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   versioning {
       enabled = true
