aa7d2a474bf98ac12d0ce973c06c11a28fcc3270
Carlos Andrés Herrera <carlos.herrera@outlook.com>
2020-04-15 10:39:02 -0500

    Revert "added kms key, added again default document to see if works"
    This reverts commit 4c2df0a1204487d3f6f13e6140a4b416f8f7aa48.
    
    Reverted due to problems with introduced changes to ssm session manager

--- terraform/ec2bastion/main.tf
+++ terraform/ec2bastion/main.tf
@@ -38,29 +38,26 @@ resource "aws_instance" "ec2_bastion" {
   }
 }
 
-resource "aws_kms_key" "ssm_session_manager_logs_key" {
-  description             = "KMS key for logs in SSM Session Manager"
-  deletion_window_in_days = 10
-}
 
-resource "aws_ssm_document" "mentors_session_manager_preferences" {
-  name            = "SSM-SessionManagerRunShell"
-  document_type   = "Session"
-  document_format = "JSON"
+# resource "aws_ssm_document" "mentors_session_manager_preferences" {
+#   name            = "SSM-SessionManagerShellPermissions"
+#   document_type   = "Session"
+#   document_format = "JSON"
 
-  content = <<DOC
-{
-  "schemaVersion":"1.0",
-  "description":"Session Document Example JSON Template",
-  "sessionType":"Standard_Stream",
-  "inputs":{
-    "s3BucketName":"${var.s3_logs_bucket}",
-    "s3KeyPrefix":"ssmLogs",
-    "s3EncryptionEnabled":true,
-    "cloudWatchLogGroupName":"/ssm/session-logs",
-    "cloudWatchEncryptionEnabled":true,
-    "kmsKeyId":"${aws_kms_key.ssm_session_manager_logs_key.id}"
-  }
-}
-DOC
-}
+#   content = <<DOC
+# {
+#     "schemaVersion": "1.0",
+#     "description": "Document to hold regional settings for Session Manager",
+#     "sessionType": "Standard_Stream",
+#     "inputs": {
+#         "s3BucketName": "${var.s3_logs_bucket}",
+#         "s3KeyPrefix": "ssmLogs",
+#         "s3EncryptionEnabled": "false",
+#         "cloudWatchLogGroupName": "/ssm/session-logs",
+#         "cloudWatchEncryptionEnabled": "false",
+#         "runAsEnabled": true,
+#         "runAsDefaultUser": "ubuntu"
+#     }
+# }
+# DOC
+# }
